a7f6c7cdb6aaa85f78d4f9ebd8107ac4cbca38c5
fabiyis <141014211+fabiyis@users.noreply.github.com>
2023-09-12 11:07:29 -0700

    playRound funtion is added, and both player&computer selection are converted to lowercase .toLowerCase

--- game.js
+++ game.js
@@ -1,8 +1,28 @@
-let choices = ["rock", "paper", "scissors"];
+const prompt = require('prompt-sync')();
 
 function getComputerChoice() {
-    let choice = Math.floor(Math.random() * choices.length);
-    return choices[choice];
+    const choices = ["rock", "paper", "scissors"]
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection,computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) 
+       {return "its is a tie";
+    }
+    else if (
+       (playerSelection === 'rock' && computerSelection === 'scissors') ||
+       (playerSelection === 'scissors' && computerSelection === 'paper') ||
+       (playerSelection === 'paper' && computerSelection ==='rock')) 
+    {
+    return 'You Won! ${playerSelection} beats ${computerSelection}';
+    }
+    else {
+        return 'You Lose! ${computerSelection} beats ${playerSelection}';
+    }
+    }
+
+    console.log(`Welcome to Rock, Paper, Scissors!`);
