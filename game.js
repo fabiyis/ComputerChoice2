89c35221bcf3365ac1ed9d134d7a428227c8680b
fabiyis <141014211+fabiyis@users.noreply.github.com>
2023-09-13 07:50:00 -0700

    initiate a game function

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
 const prompt = require('prompt-sync')();
 
 function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"]
+    let choices = ["Rock", "Paper", "Scissors"]
     let choice = Math.floor(Math.random() * choices.length);
+    console.log("Computer picked" + " " + choices[choice]);
     return choices[choice];
 }
 
@@ -12,8 +13,8 @@ function playRound(playerSelection,computerSelection) {
 
     if (playerSelection === computerSelection) 
        {return "its is a tie";
-    }
-    else if (
+
+    } else if (
        (playerSelection === 'rock' && computerSelection === 'scissors') ||
        (playerSelection === 'scissors' && computerSelection === 'paper') ||
        (playerSelection === 'paper' && computerSelection ==='rock')) 
@@ -25,3 +26,23 @@ function playRound(playerSelection,computerSelection) {
     }
     }
 
+    function game() {
+       let playerScore = 0;
+       let computerScore = 0;
+
+       for (let i = 0; i < 5; i++) {
+       const playerSelection = prompt('Enter your choice (Rock, Paper, or Scissors):');
+       const computerSelection = getComputerChoice();
+       const result = playRound(playerSelection, computerSelection);
+
+    console.log('Round ${i + 1}: ${result}');
+
+       if (result.includes('Won!')) {
+          playerScore++;
+       } else if (result.includes('Lose!')) {
+       computerScore++;
+       }
+     }
+ }
+
+console.log(game());
